27b5d6f7377391b11d362b5adb49d1abbb1a1cf1
Samuel Erwin <Samuel Erwin SREphotoMN@gmail.com>
2025-12-12 05:21:29 -0600

    feat: Add Saving Private Ryan and Iron Man scene data.

---
+++ src/data/scenes/eightiesClassics.ts
@@ -0,0 +1,61 @@
+import type { Scene } from '../types';
+
+export const terminatorScenes: Scene[] = [
+    { id: 1, title: 'Arrival', promptPayload: 'Lightning sphere appearing in alley, naked Terminator crouching, steam, night, 1984 Los Angeles street.' },
+    { id: 2, title: 'Terminator Vision', promptPayload: 'Red tinted POV, data overlay, scanning punks, selecting clothes, "Nice night for a walk".' },
+    { id: 3, title: 'Tech Noir', promptPayload: '80s night club, smoke, laser lights, Sarah Connor at table, slow motion spotting, terminator drawing laser sighted gun.' },
+    { id: 4, title: 'Police Station Attack', promptPayload: 'Terminator walking through front desk, "I\'ll be back", driving car through wall, shooting, chaos.' },
+    { id: 5, title: 'Eye Surgery', promptPayload: 'Terminator cutting out damaged eye in sink mirror, exacto knife, flesh, cyborg endoskeleton revealed.' },
+    { id: 6, title: 'Endoskeleton Rise', promptPayload: 'Burning truck wreckage, metal skeleton rising from fire, limping, red eyes glowing, horror.' },
+    { id: 7, title: 'Factory Press', promptPayload: 'Sarah crawling through hydraulic press, red button, "You\'re terminated", red eye fading out.' }
+];
+
+export const robocopScenes: Scene[] = [
+    { id: 1, title: 'Murphy\'s Death', promptPayload: 'Murphy cornered by gang, shotgun blasts, losing hand, excruciating pain, POV glitching, "Goodnight sweet prince".' },
+    { id: 2, title: 'Robocop Reveal', promptPayload: 'First view in mirror, silver visor, chin, servos whining, target acquisition, green text overlay.' },
+    { id: 3, title: 'Rape Attempt', promptPayload: 'Robocop shooting through skirt, perfect aim, "Your move creep", thugs terrified.' },
+    { id: 4, title: 'Drug Factory', promptPayload: 'Robocop busting into cocaine factory, shooting everyone, Boddicker flying through glass, white powder everywhere.' },
+    { id: 5, title: 'Boardroom', promptPayload: 'ED-209 malfunction, "You have 20 seconds to comply", tearing executive apart, panic, satire.' },
+    { id: 6, title: 'Helmet Off', promptPayload: 'Robocop removing helmet with drill, revealing human face stretched over metal, "Murphy".' },
+    { id: 7, title: 'Melting Man', promptPayload: 'Emil covered in toxic waste, melting skin, stumbling, hit by car, gory, body horror.' }
+];
+
+export const lethalWeaponScenes: Scene[] = [
+    { id: 1, title: 'Jumper', promptPayload: 'Riggs on rooftop with suicidal man, handcuffing himself, jumping off, laughing, air bag landing.' },
+    { id: 2, title: 'Firing Range', promptPayload: 'Murtaugh shooting target, Riggs shooting a smiley face, "Have a nice day", contrast in styles.' },
+    { id: 3, title: 'Desert Deal', promptPayload: 'Helicopter in desert, drug deal, shootout, Riggs crazy eyes, grenade, explosion.' },
+    { id: 4, title: 'End Fight', promptPayload: 'Riggs fighting Mr. Joshua in the rain, lawn, shirtless, martial arts, raw brawl, headlights.' },
+    { id: 5, title: 'Torture', promptPayload: 'Riggs being electrocuted, water, sponge, screaming, laughing maniacally, resilience.' },
+    { id: 6, title: 'Christmas Dinner', promptPayload: 'Riggs looking through window at Murtaugh\'s family, lonely, holiday lights, eventual invitation.' },
+    { id: 7, title: 'Traffic Jam', promptPayload: 'Murtaugh too old for this shit, 80s LA traffic, sunset, banter in the car.' }
+];
+
+export const escapeFromNewYorkScenes: Scene[] = [
+    { id: 1, title: 'Snake Plissken', promptPayload: 'Snake walking in handcuffs, eye patch, leather jacket, apathy, "Call me Snake".' },
+    { id: 2, title: 'New York Wall', promptPayload: 'Giant containment wall around Manhattan, guard towers, searchlights, dystopian night.' },
+    { id: 3, title: 'The Duke', promptPayload: 'Cadillac with chandeliers driving down Broadway, The Duke of New York, trash lined streets, weird gangs.' },
+    { id: 4, title: 'Glider Landing', promptPayload: 'Gulffire glider landing on World Trade Center, computer wireframe screen, silence, stealth.' },
+    { id: 5, title: 'Chandelier Fight', promptPayload: 'Snake fighting in the wrestling ring, spiked bats, crowd cheering, dark gritty atmosphere.' },
+    { id: 6, title: 'President Rescue', promptPayload: 'Running to the wall, taxi cab, bridge mines, tape recording, "You\'re the Duke A#1".' },
+    { id: 7, title: 'The Tape', promptPayload: 'Snake walking away, ripping the tape out of the cassette, jazz music playing, nihilistic ending.' }
+];
+
+export const bloodsportScenes: Scene[] = [
+    { id: 1, title: 'Dim Mak', promptPayload: 'Tanaka breaking the bottom brick, "Dim Mak", Frank Dux watching, asian temple setting.' },
+    { id: 2, title: 'The Split', promptPayload: 'Jean-Claude Van Damme doing the splits on two chairs, meditating, pain tolerance, flexibility.' },
+    { id: 3, title: 'Kumite Arena', promptPayload: 'Underground fighting ring, smoky, crowd shouting, diverse fighters, sweat, hong kong.' },
+    { id: 4, title: 'Chong Li', promptPayload: 'Bolo Yeung flexing pecs, "You are next!", pointing finger, scary physique, villain.' },
+    { id: 5, title: 'Coin Trick', promptPayload: 'Snatching a coin from the hand, speed, sleight of hand, amaze, "Very good, but brick not hit back".' },
+    { id: 6, title: 'Blinded Fight', promptPayload: 'Frank Dux blinded by powder, eyes closed, remembering training, sensing motion, slow motion kick.' },
+    { id: 7, title: 'Final Scream', promptPayload: 'Dux screaming, muscles tensed, victory pose, slow motion replay, 80s cheese.' }
+];
+
+export const bigTroubleScenes: Scene[] = [
+    { id: 1, title: 'Pork Chop Express', promptPayload: 'Jack Burton driving his truck, talking on CB radio, rain, "It\'s all in the reflexes".' },
+    { id: 2, title: 'Three Storms', promptPayload: 'Three elemental warriors descending, lightning, straw hats, glowing eyes, magical flying.' },
+    { id: 3, title: 'Lo Pan', promptPayload: 'David Lo Pan in wheelchair, old man, creepy fingernails, neon green magic light.' },
+    { id: 4, title: 'Alley Fight', promptPayload: 'Chang Sing vs Wing Kong, gang war in alley, guns vs kung fu, chaos.' },
+    { id: 5, title: 'Guardian Eye', promptPayload: 'Floating eyeball monster, spying, disgusting, veins, practical effects.' },
+    { id: 6, title: 'Lipstick Kiss', promptPayload: 'Jack Burton with bright red lipstick on his face, confused, "What the hell is going on?".' },
+    { id: 7, title: 'Exploding Guy', promptPayload: 'Thunder swelling up, red face, steam, exploding in green goop, shock.' }
+];
